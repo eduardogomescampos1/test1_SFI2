a558761035c9ab6808e7c3369fd787b333c2524f
Eduardo <eduardogc0303@usp.br>
2024-02-28 10:18:20 -0300

    Fixing test file

--- test1_regular.sh
+++ test1_regular.sh
@@ -1,36 +1,36 @@
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
+sleep 10 
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
 sleep 10
-sudo stress --cpu 3 --timeout 10
-sleep 10
-sudo stress --cpu 3 --timeout 10
+stress --cpu 3 --timeout 10
