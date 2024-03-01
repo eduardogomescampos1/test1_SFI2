5f878cfa1e17f3a76b56bc6af834599e6567eec0
Eduardo <eduardogc0303@usp.br>
2024-03-01 15:10:27 -0300

    Setting up lower stress for periodic test

--- test1_regular.sh
+++ test1_regular.sh
@@ -1,6 +1,46 @@
 sleep 60
-stress --cpu 3 --timeout 60
+stress --io 5 --timeout 60
 sleep 60
-stress --cpu 3 --timeout 60
+stress --io 5 --timeout 60
 sleep 60
-stress --cpu 3 --timeout 60
+stress --io 5 --timeout 60
+sleep 60
+stress --io 5 --timeout 60
+sleep 60
+stress --io 5 --timeout 60
+sleep 60
+stress --io 5 --timeout 60
+sleep 60
+stress --io 5 --timeout 60
+sleep 60
+stress --io 5 --timeout 60
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
