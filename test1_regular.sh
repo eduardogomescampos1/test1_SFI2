5824337e91fba6d927337a3a830fa5587c466598
Eduardo <eduardogc0303@usp.br>
2024-03-05 15:34:57 -0300

    Lowering the duration of the test

--- test1_regular.sh
+++ test1_regular.sh
@@ -4,16 +4,6 @@ sleep 60
 stress --io 15 --timeout 60
 sleep 60
 stress --io 15 --timeout 60
-sleep 60
-stress --io 15 --timeout 60
-sleep 60
-stress --io 15 --timeout 60
-sleep 60
-stress --io 15 --timeout 60
-sleep 60
-stress --io 15 --timeout 60
-sleep 60
-stress --io 15 --timeout 60
 
 
 
