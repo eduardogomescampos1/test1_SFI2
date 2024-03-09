c25db79d9e820997b38661a90c8bcc10699c6a91
eduardogomescampos <eduardogc0303@usp.br>
2024-03-09 20:57:11 -0300

    Fixing doubled variables

--- python_statistics/nonlinear.py
+++ python_statistics/nonlinear.py
@@ -18,10 +18,10 @@ for i in rows:
     y.append((i[1]))
 x = [float(i) for i in x] 
 y = [float(i) for i in y]
-x_array =x
+x_array = x
 y_array = y
 popt, pcov = curve_fit(non_linear,x_array,y_array)
-plt.scatter(x_array,y,label = 'Sample data') # x and y will be read from a csv file on the actual project
+plt.scatter(x_array,y,label = 'Sample data') # x and y will be read from a csv file output by Grafana on the actual project
 plt.plot(x_array, non_linear(x_array, *popt), 'r-', label= 'Regression')
 plt.legend()
 plt.savefig('plot.png')
