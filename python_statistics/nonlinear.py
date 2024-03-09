d44697425d251bc82af6fb763b24211ea1952c6b
eduardogomescampos <eduardogc0303@usp.br>
2024-03-09 19:41:53 -0300

    Adding statistics to create a model to fit our data

---
+++ python_statistics/nonlinear.py
@@ -0,0 +1,28 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+def non_linear(x,a,b):
+    return a +b*np.power(x,1./3)
+
+csvreader = csv.reader(open('dumb.csv'))
+x = []
+y = []
+drop = next(csvreader)
+rows = []
+for row in csvreader:
+    rows.append(row)
+for i in rows:
+    x.append((i[0]))
+    y.append((i[1]))
+x = [float(i) for i in x] 
+y = [float(i) for i in y]
+x_array =x
+y_array = y
+popt, pcov = curve_fit(non_linear,x_array,y_array)
+plt.scatter(x_array,y,label = 'Sample data') # x and y will be read from a csv file on the actual project
+plt.plot(x_array, non_linear(x_array, *popt), 'r-', label= 'Regression')
+plt.legend()
+plt.savefig('plot.png')
+print(popt)
